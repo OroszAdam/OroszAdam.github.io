1adfb56ecda319d1c44110a2c610da7f2fcd916e
OroszAdam <alteradamo@gmail.com>
2022-12-25 11:44:51 +0100

    WIP: video added

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -10,12 +10,31 @@ export default class Room {
     this.room = this.resources.items.room;
     this.actualRoom = this.room.scene;
 
+    console.log(this.actualRoom.children);
     this.setModel();
   }
 
   setModel() {
+    this.actualRoom.children.forEach((child) => {
+      child.castShadow = true;
+      child.receiveShadow = true;
+      if (child instanceof THREE.Group) {
+        child.children.forEach((groupchild) => {
+          groupchild.castShadow = true;
+          groupchild.receiveShadow = true;
+        });
+      }
+      if (child.name === "Screen") {
+        child.material = new THREE.MeshBasicMaterial({
+          map: this.resources.items.screen,
+        });
+        console.log(child.material);
+      }
+    });
+
     this.scene.add(this.actualRoom);
-    console.log(this.actualRoom);
+    // in case scaling is needed:
+    //this.actualRoom.scale.set(0.1, 0.1, 0.1);
   }
   resize() {}
   update() {}
