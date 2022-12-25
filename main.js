897bc8870115c782743c0ee5379acbd33c517845
OroszAdam <alteradamo@gmail.com>
2022-12-25 15:25:40 +0100

    github host error fix (?)

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import "./style.css";
+import "./style.css" assert { type: "css" };
 import Experience from "./Experience/Experience";
 const experience = new Experience(document.querySelector(".experience-canvas"));
